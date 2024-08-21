ad46db071f853c55de931c19eb437fade247588c
osama722004 <zdrosama@gmail.com>
2024-08-22 02:44:51 +0300

    Add all my files

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import cors
 const authRoutes = require("./routes/auth");
-const cors = require("cors");
-app.use(cors());
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI =
   "mongodb+srv://osamazaid821:6OTZWIVxujIf7qbn@cluster0.uiq1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 // Middleware
+app.use(cors()); // Use cors middleware
 app.use(express.json());
 
 // Routes
@@ -17,20 +17,8 @@ app.use("/api/auth", authRoutes);
 
 // Connect to MongoDB and start the server
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
-  .catch((err) => console.error("err"));
-//6OTZWIVxujIf7qbn pass
-//osamazaid821 user
-/**
- * 
- * 
- * {
-  "username": "exampleUser",
-  "email": "example@example.com",
-  "password": "examplePassword"
-}
-
- */
+  .catch((err) => console.error("Database connection error:", err));
