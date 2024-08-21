dc8329fa6e2b1754330bc1023d3eee7365a62c70
osama722004 <zdrosama@gmail.com>
2024-08-22 02:37:57 +0300

    Add all my files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
+const cors = require("cors");
+app.use(cors());
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
